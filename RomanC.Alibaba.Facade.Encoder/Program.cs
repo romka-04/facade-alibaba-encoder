aa2ce30393d1f486baf03b7a86ee16b4c49134c0
Roman Chernobrovkin <roman.chernobrovkin@myib.com>
2021-06-12 21:20:23 +0300

    Change to one project (instead of Client, Server)

--- RomanC.Alibaba.Facade.Encoder/Program.cs
+++ RomanC.Alibaba.Facade.Encoder/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RomanC.Alibaba.Facade.Encoder.Client
+namespace RomanC.Alibaba.Facade.Encoder
 {
     public class Program
     {
